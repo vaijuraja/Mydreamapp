49dfd34c53befa783f381d41df4801d3b0f7c2d7
vaijuraja <46179583+vaijuraja@users.noreply.github.com>
2021-02-01 21:57:43 +0530

    mydreamapp

---
+++ Angular/my-dream-app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'design-app';
+  myimage1:string="assets/images/1stimg.png";
+  myimage2:string="assets/images/Low_interest_rate.png";
+  myimage3:string="assets/images/freelancer_feature.png";
+  myimage4:string="assets/images/Secure_Payments.png";
+  myimage5:string="assets/images/Covid.png";
+  myimage6:string="assets/images/collect_img_video.png";
+  myimage7:string="assets/images/logo.png";
+  myimage8:string="assets/images/circle.svg";
+  myimage9:string="assets/images/blob-small.svg";
+  myimage10:string="assets/images/accounting_dots.svg";
+
+}
